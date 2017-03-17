fce6cbdda66618dbb12abb5e4825c3a6496b4941
JinHo <lece619@gmail.com>
2017-03-17 13:23:32 +0900

    StudyStart

---
+++ app/src/main/java/com/example/user/androidstudy/StudyPage.java
@@ -0,0 +1,13 @@
+package com.example.user.androidstudy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StudyPage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study_page);
+    }
+}
